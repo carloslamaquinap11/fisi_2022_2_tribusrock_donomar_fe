b5c4ae83d5cfa29c26757e81aec9ed77e0db3ddb
FRANCO KHALED TELLO PALACIOS <franco.tello@unmsm.edu.pe>
2023-01-01 22:04:25 -0500

    2204

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,4 +1,5 @@
 export 'package:fisi_2022_2_tribusrock_donomar_fe/screens/alert_screen.dart';
 export 'package:fisi_2022_2_tribusrock_donomar_fe/screens/login_screen.dart';
 export 'package:fisi_2022_2_tribusrock_donomar_fe/screens/registro_screen.dart';
+export 'package:fisi_2022_2_tribusrock_donomar_fe/screens/registro_cita_screen.dart';
 export 'package:fisi_2022_2_tribusrock_donomar_fe/screens/home_screen.dart';
