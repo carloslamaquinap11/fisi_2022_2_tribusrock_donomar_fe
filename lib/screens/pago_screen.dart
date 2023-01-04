6a7930cfd04599b4bb568600856eea902df4fd13
ErickxandeR99 <erick.estrada@unmsm.edu.pe>
2023-01-04 04:14:50 -0500

    pagoscreen terminado

--- lib/screens/pago_screen.dart
+++ lib/screens/pago_screen.dart
@@ -4,17 +4,22 @@ import 'package:fisi_2022_2_tribusrock_donomar_fe/screens/home_screen.dart';
 // import 'package:fisi_2022_2_tribusrock_donomar_fe/widgets/custom_medical_button.dart';
 // import 'package:fisi_2022_2_tribusrock_donomar_fe/widgets/custom_next_appointment_button.dart';
 import 'package:fisi_2022_2_tribusrock_donomar_fe/widgets/widgets.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Pago extends StatelessWidget {
-  const Pago({Key? key}) : super(key: key);
+ 
+   Pago({Key? key}) : super(key: key);
   static final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
   static final Map<String, String> formValues = {
     'email': 'micorreo@gmail.com',
     'password': '123123123'
   };
+  var cardMask = MaskTextInputFormatter(mask: '####-####-####-####',filter: {"#": RegExp(r'[0-9]')});
+  var dateMask = MaskTextInputFormatter(mask: '##/##',filter: {"#": RegExp(r'[0-9]')});
+  var codeMask = MaskTextInputFormatter(mask: '###',filter: {"#": RegExp(r'[0-9]')});
 
   @override
   Widget build(BuildContext context) {
@@ -59,45 +64,76 @@ class Pago extends StatelessWidget {
                     height: 15,
                    ),
 
-                    CustomInputField(
-                      labelText: 'Correo',
-                      keyboardType: TextInputType.emailAddress,
-                      formProperty: 'email',
-                      formValues: formValues,
-                    ),
-                   
+                    inputCorreo(),
+
                    SizedBox(
                     height: 10,
                    ),
 
-                    CustomInputField(
-                      labelText: 'Numero de cuenta',
-                      isPassword: true,
-                      formProperty: 'password',
-                      formValues: formValues,
-                    ),
+                   inputCard(),
+
+                  SizedBox(
+                    height: 10,
+                   ),
+
+                   Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width*0.3,
+                        child: inputDate() ,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width*0.3,
+                        child: inputCode() ,
+                      ),
+
+                    ],
+                   ),
                     
-                    SizedBox(
-                    height: 10,
-                   ),
+                    
+                    
+                    // CustomInputField(
+                    //   labelText: 'Correo',
+                    //   keyboardType: TextInputType.emailAddress,
+                    //   formProperty: 'email',
+                    //   formValues: formValues,
+                    // ),
                    
-                   CustomInputField(
-                      labelText: 'Fecha',
-                      keyboardType: TextInputType.emailAddress,
-                      formProperty: 'email',
-                      formValues: formValues,
-                    ),
+                  //  SizedBox(
+                  //   height: 10,
+                  //  ),
+
+                  //   CustomInputField(
+                  //     labelText: 'Numero de cuenta',
+                  //     isPassword: true,
+                  //     formProperty: 'password',
+                  //     formValues: formValues,
+                  //   ),
+                    
+                  //   SizedBox(
+                  //   height: 10,
+                  //  ),
                    
-                    SizedBox(
-                    height: 10,
-                   ),
+                  //  CustomInputField(
+                  //     labelText: 'Fecha',
+                  //     keyboardType: TextInputType.emailAddress,
+                  //     formProperty: 'email',
+                  //     formValues: formValues,
+                  //   ),
                    
-                   CustomInputField(
-                      labelText: 'Numero',
-                      keyboardType: TextInputType.emailAddress,
-                      formProperty: 'email',
-                      formValues: formValues,
-                    ),
+                  //   SizedBox(
+                  //   height: 10,
+                  //  ),
+                   
+                  //  CustomInputField(
+                  //     labelText: 'Numero',
+                  //     keyboardType: TextInputType.emailAddress,
+                  //     formProperty: 'email',
+                  //     formValues: formValues,
+                  //   ),
 
 
                     SizedBox(
@@ -131,6 +167,82 @@ class Pago extends StatelessWidget {
       ),
     );
     throw UnimplementedError();
+  }
+
+  Container inputCorreo() {
+    return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white)
+                    ),
+                    child: TextFormField(
+                      style: const TextStyle(fontSize: 20, color: Color(0xFF2196F3)),
+                      decoration: const InputDecoration(
+                        hintText: "Correo Electronico",
+                      ),
+                    ),
+
+                  );
+  }
+
+
+  Container inputCard() {
+    return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white)
+                    ),
+                    child: TextFormField(
+                      inputFormatters: [cardMask],
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        hintText: "0000-0000-0000-0000",
+                      ),
+                    ),
+
+                  );
+  }
+
+  Container inputDate() {
+    return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white)
+                    ),
+                    child: TextFormField(
+                       inputFormatters: [dateMask],
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        hintText: "01/24",
+                      ),
+                    ),
+                    
+                  );
+  }
+
+
+Container inputCode() {
+    return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white)
+                    ),
+                    child: TextFormField(
+                      inputFormatters: [codeMask],
+                      keyboardType: TextInputType.number,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        hintText: "123",
+                      ),
+                    ),
+
+                  );
   }
 }
 
